64faae344453a32fb1a81530c85d86d505f20abd
muhrizky-a <82527236+muhrizky-a@users.noreply.github.com>
2021-06-08 06:49:29 +0800

    Version 1.0.0 reset directory

--- lib/global_variables.dart
+++
@@ -1,8 +0,0 @@
-
-import 'model/user.dart';
-
-//Masukkan endpoint backend disini
-String hostUrl = "https://mlwrmn-event.000webhostapp.com/script";
-
-//Objek yang menyimpan data user saat login
-User currentUser = new User();
